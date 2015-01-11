f8ae64fdac7bdb2ec8774f821c5c8e47177f4f3b
Mark Scherer <euromark@web.de>
2015-01-11 03:42:25 +0100

    migration

---
+++ config/Migrations/20150111023838_init.php
@@ -0,0 +1,45 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class Init extends AbstractMigration {
+
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     *
+     * Uncomment this method if you would like to use it.
+     *
+    public function change()
+    {
+    }
+    */
+
+    /**
+     * Migrate Up.
+     */
+    public function up() {
+			$this->execute("CREATE TABLE IF NOT EXISTS `tokens` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) DEFAULT NULL,
+  `type` varchar(10) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `key` varchar(60) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `content` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `used` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `unlimited` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;");
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down() {
+			$this->execute('DROP table tokens');
+    }
+}
--- config/bootstrap.php
+++ config/bootstrap.php
@@ -183,11 +183,12 @@ Request::addDetector('tablet', function ($request) {
  *
  */
 
-//Plugin::load('Migrations');
+Plugin::load('Migrations');
 
 // Only try to load DebugKit in development mode
 // Debug Kit should not be installed on a production system
 if (Configure::read('debug')) {
+	// Deactivated to not mess up the logging
 	//Plugin::load('DebugKit', ['bootstrap' => true]);
 }
 
